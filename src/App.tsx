3578f22f17e37095660b690041ccdba27e9f9c15
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-12-01 10:19:51 +0330

    load google map using dynamic library import

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
-import { APIProvider, Map } from "@vis.gl/react-google-maps";
-import ControlButtonsWrapper from "./components/ControlButtonWrapper";
+import { useEffect } from "react";
+import "./App.css";
 
 const App = () => {
+  // useEffect(() => {
+  //   const loadGoogleMaps = async () => {
+  //     if (!document.querySelector("script[src*='maps.googleapis.com']")) {
+  //       const googleMapsScript = document.createElement("script");
+  //       googleMapsScript.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyBLZxDKEynXZdwnrfwiLvi6UjkOew7i8-Y&libraries=places`;
+  //       googleMapsScript.async = true;
+  //       googleMapsScript.defer = true;
+
+  //       googleMapsScript.onload = async () => {
+  //         const { Map } = (await google.maps.importLibrary(
+  //           "maps"
+  //         )) as google.maps.MapsLibrary;
+
+  //         new Map(document.getElementById("map") as HTMLElement, {
+  //           center: { lat: -34.397, lng: 150.644 },
+  //           zoom: 8,
+  //         });
+  //       };
+
+  //       document.head.appendChild(googleMapsScript);
+  //     }
+  //   };
+
+  //   loadGoogleMaps();
+  // }, []);
+
+  useEffect(() => {
+    const initMap = async () => {
+      try {
+        // Dynamically import the "maps" library
+        const { Map } = (await google.maps.importLibrary(
+          "maps"
+        )) as google.maps.MapsLibrary;
+
+        // Initialize the map
+        new Map(document.getElementById("map") as HTMLElement, {
+          center: { lat: -34.397, lng: 150.644 },
+          zoom: 8,
+        });
+      } catch (error) {
+        console.error("Failed to initialize the map:", error);
+      }
+    };
+
+    initMap();
+  }, []);
+
   return (
-    <div>
-      <APIProvider apiKey="AIzaSyBLZxDKEynXZdwnrfwiLvi6UjkOew7i8-Y">
-        <Map
-          style={{ width: "100vw", height: "100vh" }}
-          defaultCenter={{ lat: 22.54992, lng: 0 }}
-          defaultZoom={3}
-          gestureHandling={"greedy"}
-          disableDefaultUI={true}
-        >
-          <ControlButtonsWrapper />
-        </Map>
-      </APIProvider>
-    </div>
+    <div
+      id="map"
+      style={{
+        width: "100%", // Full width
+        height: "100vh", // Full height
+      }}
+    ></div>
   );
 };
 
 export default App;
-
-//  @vis.gl/react-google-maps docs: https://visgl.github.io/react-google-maps/docs
