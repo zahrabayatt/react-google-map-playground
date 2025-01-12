769378ed3e544056c0946136e99fb5adf803e80e
Zahra Bayat <zahra.bayat13799@gmail.com>
2025-01-12 20:34:05 +0330

    Add wrapper google map sample

--- src/components/GoogleMap.tsx
+++ src/components/GoogleMap.tsx
@@ -1,20 +1,13 @@
 import { useEffect, useRef, useState } from "react";
-import { GoogleMapsOverlay } from "@deck.gl/google-maps";
-import useBartStationsLayer from "../hooks/useBartStationsLayer";
-import useZipCodeLayer from "../hooks/useZipCodeLayer";
 
 interface Props {
   center: google.maps.LatLngLiteral;
   zoom: number;
-  panToMarker: () => void;
 }
 
-const GoogleMap = ({ center, zoom, panToMarker }: Props) => {
+const GoogleMap = ({ center, zoom }: Props) => {
   const ref = useRef<HTMLDivElement>(null);
   const [map, setMap] = useState<google.maps.Map | null>(null);
-  const [marker, setMarker] = useState<google.maps.Marker | null>(null);
-  const bartStationsLayer = useBartStationsLayer();
-  const zipCodesLayer = useZipCodeLayer();
 
   useEffect(() => {
     if (ref.current) {
@@ -23,51 +16,14 @@ const GoogleMap = ({ center, zoom, panToMarker }: Props) => {
         zoom,
       });
       setMap(mapInstance);
-
-      const markerInstance = new window.google.maps.Marker({
-        position: center,
-        map: mapInstance,
-        title: "Uluru",
-      });
-      setMarker(markerInstance);
     }
   }, [center, zoom]);
 
-  useEffect(() => {
-    if (map && marker) {
-      map.panTo(marker.getPosition() as google.maps.LatLng);
-      map.setZoom(8);
-    }
-  }, [center, map, marker]);
-
-  useEffect(() => {
-    if (map && bartStationsLayer && zipCodesLayer) {
-      const overlay = new GoogleMapsOverlay({
-        layers: [bartStationsLayer, zipCodesLayer],
-      });
-      overlay.setMap(map);
-    }
-  }, [map, bartStationsLayer, zipCodesLayer]);
 
   return (
     <div style={{ position: "relative" }}>
       <div ref={ref} id="map" style={{ width: "100%", height: "100vh" }}></div>
-      <button
-        onClick={panToMarker}
-        style={{
-          position: "absolute",
-          bottom: "40px",
-          left: "10px",
-          zIndex: 10,
-        }}
-      >
-        Pan to Uluru
-      </button>
     </div>
   );
 };
 export default GoogleMap;
-
-// https://deck.gl/docs/get-started/getting-started
-// https://deck.gl/docs/api-reference/layers/scatterplot-layer#
-// https://deck.gl/docs/api-reference/google-maps/overview
