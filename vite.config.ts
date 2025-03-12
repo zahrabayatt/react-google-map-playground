22f85746be86a5c2da54d5c667b73f067f9e8ae5
Zahra Bayat <zahra.bayat13799@gmail.com>
2025-03-13 01:06:36 +0330

    Enhance GitHub Actions workflow for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/react-google-map-playground/",
-  build: {
-    assetsDir: "assets",
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
 });
